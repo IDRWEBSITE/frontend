a7944ffd7e589d0d1b8b22dbfc0f53fc6fae5371
mohitpanchal09 <panchalmohitg2002@gmail.com>
2023-10-10 13:55:51 +0530

    added  drone parallex

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -5,12 +5,13 @@ import { useEffect } from "react";
 import Plx from "react-plx";
 import background from "../../assets/Background.png";
 import mountain from "../../assets/Mountain.png";
+import drone2 from "../../assets/drone2.png";
 import AOS from "aos";
 import "aos/dist/aos.css";
 const Home = () => {
-  useEffect(() => {
-    AOS.init({ duration: 3000 });
-  }, []);
+  AOS.init({
+    duration: 2000,
+  });
 
   return (
     <>
@@ -82,10 +83,101 @@ const Home = () => {
           >
             <img src={mountain} alt="background" />
           </Plx>
+          <Plx
+            className="MyAwesomeParallax"
+            parallaxData={[
+              {
+                start: 0,
+
+                duration: 800,
+                properties: [
+                  {
+                    startValue: -800,
+                    endValue: 500,
+                    property: "translateX",
+                  },
+                ],
+              },
+              {
+                start: 500,
+
+                duration: 300,
+
+                properties: [
+                  {
+                    startValue: 0,
+                    endValue: -800,
+                    property: "translateX",
+                  },
+                ],
+              },
+            ]}
+          >
+            <div style={{ position: "absolute", bottom: "10px" }}>
+              <h1
+                style={{
+                  color: "white",
+                  fontSize: "8rem",
+                  margin: "0 20px",
+                }}
+              >
+                DOOT MK-1
+              </h1>
+            </div>
+          </Plx>
+          <Plx
+            className="MyAwesomeParallax"
+            parallaxData={[
+              {
+                start: 0,
+
+                duration: 500,
+                properties: [
+                  {
+                    startValue: 1,
+                    endValue: 1.05,
+                    property: "scale",
+                  },
+                  {
+                    startValue: 100,
+                    endValue: -200,
+                    property: "translateY",
+                  },
+                ],
+              },
+              {
+                start: 600,
+
+                duration: 900,
+                easing: [0.25, 0.1, 0.53, 3],
+                properties: [
+                  {
+                    startValue: 1,
+                    endValue: -10,
+                    property: "opacity",
+                  },
+                ],
+              },
+            ]}
+          >
+            <div style={{ position: "relative" }}>
+              <img
+                src={drone2}
+                alt=""
+                srcset=""
+                style={{ position: "fixed" }}
+              />
+            </div>
+          </Plx>
         </div>
 
         <div style={{}}>
-          <img src={mountain} alt="background" style={{}} />
+          <img
+            src={mountain}
+            alt="background"
+            style={{}}
+            className={"fade-right"}
+          />
         </div>
       </div>
     </>
