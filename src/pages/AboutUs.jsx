6dc9737453f76affc7c3f1807f2e5212507df92e
mohitpanchal09 <panchalmohitg2002@gmail.com>
2023-11-04 18:37:08 +0530

    fixed merge conflicts

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,21 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
 import Footer from "../components/Footer/Footer";
-import Testimonials from "../components/testimonials/Testimonials";
+import Testimonials from "../components/testimonials/testimonials";
 import AboutComponent from "../components/AboutComponent/AboutComponent";
 const AboutUs = () => {
   return (
     <div className="bg-black w-[100vw] h-[100vh]">
-<<<<<<< HEAD
-    <Navbar />
-    <AboutComponent />
-     <Footer />
-=======
       <Navbar />
       <AboutComponent />
-      {/* <Testimonials /> */}
       <Footer />
->>>>>>> 7ef0fdb (new home page)
     </div>
   );
 };
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,23 +1,3 @@
-<<<<<<< HEAD
-import React from 'react'
-import NavBar from '../components/Navbar-2/NavBar'
-import Footer from '../components/Footer/Footer'
-import Home from '../components/Home/Home'
-import BG from '../components/Doot/bg'
-import KeyFeatures from '../components/KeyFeatures/KeyFeatures'
-import Testimonials from '../components/testimonials/testimonials'
-const HomePage = () => {
-  return (
-    <div>
-      {/* <NavBar />
-      <Home /> */}
-{/*       <NavBar />
-      <Home /> */}
-      {/* <KeyFeatures/>
-      <BG/>
-      <Testimonials/> */}
-
-=======
 import React from "react";
 import NavBar from "../components/Navbar-2/NavBar";
 import Footer from "../components/Footer/Footer";
@@ -30,7 +10,6 @@ const HomePage = () => {
       <NavBar />
       <Home />
       <Footer />
->>>>>>> 7ef0fdb (new home page)
     </div>
   );
 };
