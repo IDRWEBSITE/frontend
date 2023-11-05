8836ae652f5490a8adfb5dd1ea1e0ab23196c4d1
dhruvalgupta2003 <dhruvalgupta2003@gmail.com>
2023-11-05 12:51:23 +0530

    navbar

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-import Navbar from "../components/Navbar/Navbar";
+import Navbar from "../components/Navbar-2/NavBar";
 import Footer from "../components/Footer/Footer";
 import Home from "../components/Home/Home";
 
 const HomePage = () => {
   return (
     <>
-      <Navbar />
       <div>
+        <Navbar />
         <Home />
         <Footer />
       </div>
